8412c631b90603f3ee6651d364b4dd88ab41892d
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2024-01-28 13:50:04 +0000

    Added db connection

---
+++ sql/queries.sql
@@ -0,0 +1,14 @@
+use `projeto-ti`;
+
+-- CREATE DATABASE IF NOT EXISTS projet-ti
+-- CREATE TABLE IF NOT EXISTS users (
+--    user_id INT AUTO_INCREMENT PRIMARY KEY,
+--    username VARCHAR(50) NOT NULL,
+--    email VARCHAR(100) NOT NULL,
+--    password VARCHAR(255) NOT NULL,
+--    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+INSERT INTO
+    users (username, email, password)
+VALUES
+    ('john_doe', 'john.doe@example.com', 'lol123');
---
+++ src/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php
+require_once('./php/cnn.php')
+?>
+
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<!-- CSS  -->
+	<link rel="stylesheet" href="./css/styles.css" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+
+	<!-- JS -->
+	<script defer src="scripts/index.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+	<title>Projeto TI</title>
+</head>
+
+<body></body>
+
+</html>
+<?php
+$pdo = null;
+?>
