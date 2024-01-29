ca9242ec93aed14785b76caef05f10849e4238b9
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2024-01-29 10:54:09 +0000

    Better password rules

--- src/includes/signup/signup.inc.php
+++ src/includes/signup/signup.inc.php
@@ -36,6 +36,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $errors["invalid_password"] = "Passwords do not match";
         }
 
+        if (strlen($password) < 6) {
+            $errors["short_password"] = "Password must be at least 6 characters long";
+        }
+
         if (strtotime($birthdate) >= time()) {
             $errors["invalid_birthdate"] = "Invalid Birthdate";
         }
